b04809fe097940bcbacb90c9f8c1c11e19bf9994
Ravinda Lakshan <ravindalakshan@gmail.com>
2025-06-13 22:42:55 +0300

    Initial commit of spring ai project

---
+++ src/main/java/com/ai/smartCoder/SmartCoderApplication.java
@@ -0,0 +1,13 @@
+package com.ai.smartCoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/smartCoder/SmartCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.smartCoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
